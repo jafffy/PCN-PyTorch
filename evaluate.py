e4ddeaa8d1d6eae86f6e53d0a21330f208ff69e8
SungminLee <starmin114@yonsei.ac.kr>
2022-01-11 07:55:34 +0900

    First evaluation

--- evaluate.py
+++ evaluate.py
@@ -1,15 +1,15 @@
 import argparse
 import torch
-
-from dataset.dataset import ShapeNet
+import os
+from dataset.dataset import S3DIS
 from model import AutoEncoder
 from loss import ChamferDistance
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--partial_root', type=str, default='/home/rico/Workspace/Dataset/shapenetpcn/partial')
-parser.add_argument('--gt_root', type=str, default='/home/rico/Workspace/Dataset/shapenetpcn/gt')
-parser.add_argument('--batch_size', type=int, default=16)
+parser.add_argument('--complete_path', type=str, default="/home/ubuntu/Projects/PCD_completion/dataset/S3DIS/processed/area1_pruned4_complete.pcv")
+parser.add_argument('--incomplete_path', type=str, default="/home/ubuntu/Projects/PCD_completion/dataset/S3DIS/processed/area1_pruned4_incomplete_seed_10_0.2.pcv")
+parser.add_argument('--batch_size', type=int, default=1)
 parser.add_argument('--num_workers', type=int, default=4)
 args = parser.parse_args()
 
@@ -17,11 +17,11 @@ DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 
 cd_loss = ChamferDistance()
 
-test_dataset = ShapeNet(partial_path=args.partial_root, gt_path=args.gt_root, split='test')
+test_dataset = S3DIS(incomplete_path=args.incomplete_path, complete_path=args.complete_path, split='test')
 test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
 
 network = AutoEncoder()
-network.load_state_dict(torch.load('log/lowest_loss.pth'))
+network.load_state_dict(torch.load('log/pretrained_model.pth'))
 network.to(DEVICE)
 
 # testing: evaluate the mean cd loss
@@ -29,18 +29,17 @@ network.eval()
 with torch.no_grad():
     total_loss, iter_count = 0, 0
     for i, data in enumerate(test_dataloader, 1):
-        partial_input, coarse_gt, dense_gt = data
+        incomplete_input, complete_input = data
+
+        incomplete_input = incomplete_input.to(DEVICE)
+        complete_input = complete_input.to(DEVICE)
+        incomplete_input = incomplete_input.permute(0, 2, 1)
         
-        partial_input = partial_input.to(DEVICE)
-        coarse_gt = coarse_gt.to(DEVICE)
-        dense_gt = dense_gt.to(DEVICE)
-        partial_input = partial_input.permute(0, 2, 1)
-        
-        v, y_coarse, y_detail = network(partial_input)
+        v, y_coarse, y_detail = network(incomplete_input)
 
         y_detail = y_detail.permute(0, 2, 1)
 
-        loss = cd_loss(dense_gt, y_detail)
+        loss = cd_loss(complete_input, y_detail)
         total_loss += loss.item()
         iter_count += 1
 
